9ad8f871a01f84523899d3d0b9d23b97c8146bb0
Dima Chemerevsky <dchemerevsky@aras.com>
2024-03-11 23:32:10 +0100

    NodeService. Correst default port according to requirements

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './httpExceptionFilter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(parseInt(process.env.PORT, 10) || 3000);
+  await app.listen(parseInt(process.env.PORT, 10) || 4000);
 }
 bootstrap();
